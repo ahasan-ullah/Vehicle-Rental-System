184f6efb9a7c04964cd3f151ee7265f40d1ae33e
ahasan-ullah <ahasanullah451@gmail.com>
2025-12-07 18:49:19 +0600

    role based authorization added

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -2,20 +2,28 @@ import { NextFunction, Request, Response } from "express"
 import jwt, { JwtPayload } from "jsonwebtoken";
 import config from "../config";
 
-const auth=()=>{
+const auth=(...roles:string[])=>{
   return async (req:Request,res:Response,next:NextFunction)=>{
     try {
       const token=req.headers.authorization;
 
       if(!token){
         return res.status(500).json({
-          message: "unathorized access"
+          message: "unathorized access (not logged in)"
         })
       }
 
-      const decoded=jwt.verify(token,config.secret_key as string);
-      req.user=decoded as JwtPayload;
+      const decoded=jwt.verify(token.split(" ")[1] as string,config.secret_key as string) as JwtPayload;
+      req.user=decoded;
 
+      console.log(decoded)
+
+
+      if(roles.length && !roles.includes(decoded.user.role)){
+        return res.status(500).json({
+          error: "unathorized access"
+        })
+      }
       next();
     } catch (error:any) {
       res.status(500).json({
--- src/modules/user/user.routes.ts
+++ src/modules/user/user.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { userControllers } from "./user.controllers";
+import auth from "../../middleware/auth";
 
 const router=Router();
 
-router.get('/',userControllers.getAllUsers);
+router.get('/',auth("admin"), userControllers.getAllUsers);
 
 
 export const userRoutes=router;
